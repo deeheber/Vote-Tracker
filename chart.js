ee2fcbccb10597daae6c12a4df4d656219603590
Danielle H <daheber@gmail.com>
2015-11-25 11:25:02 -0800

    Button and Label display randomly for vote options

--- chart.js
+++ chart.js
@@ -14,9 +14,31 @@
   /***    Chart Object ***/
   var chart = null;
 
-  function changeRed() {
-    colorData[2].y++;
+  function vote(index) {
+    colorData[index].y++;
     chart.render();
+    newChoices();
+
+  }
+
+  function newChoices() {
+    /***Generate two random numbers***/
+      var randomNum1 = Math.floor((Math.random() * 10) + 1);
+      var randomNum2 =  Math.floor((Math.random() * 10) + 1);
+
+    /***Check to make sure the two numbers aren't the same***/
+      while (randomNum1 == randomNum2) {
+        randomNum1 = Math.floor((Math.random() * 10) + 1);
+        randomNum2 =  Math.floor((Math.random() * 10) + 1);
+      }
+    /***Write the two choices to the page**/
+      var choice1 = document.getElementById("choice1");
+      choice1.innerHTML = "<p>" + colorData[randomNum1].label + "</p>";
+      choice1.innerHTML+= "<button onclick='vote("+randomNum1+")'>Pick me</button>";
+
+      var choice2 = document.getElementById("choice2");
+      choice2.innerHTML = "<p>" + colorData[randomNum2].label + "</p>";
+      choice2.innerHTML+= "<button onclick='vote("+randomNum2+")'>Pick me</button>";
   }
 
   window.onload = function () {
