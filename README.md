4115dc2aa9ec4aad9bf10e6c5d78488b4a6dd0db
Danielle H <daheber@gmail.com>
2015-11-25 12:20:11 -0800

    Got it working for the most part

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 # Vote-Tracker
 This is an app that allows users to vote on their preferred item, stores the vote, and displays the totals in a graph using the canvasJS library.
 
-Vote Tracker Components:
-
-Object: Favorite
-Properties: Image, Name/Label, Votes/y
-
-Array: Favorite Items
-
-Process:
-    Display 2 random items (ensure that 2 of the same item isn't displayed at the same time)
-    Record favorite vote
-        capture the click event on the pictures
-        update the vote total for the clicked item by incrementing it by 1
-    Update the chart
-        be sure to call chart.render()
-    Repeat process
+Questions:
+1. Randomly getting uncaught TypeError: Cannot read property 'image' of undefined.
+2. Why is the height of my #chartContainer section's height 0?
--- chart.js
+++ chart.js
@@ -1,16 +1,16 @@
 
   /***    Data Points ***/
   var colorData = new Array();
-  colorData.push({ label: "red", y: 18 });
-  colorData.push({ label: "orange", y: 29 });
-  colorData.push({ label: "yellow", y: 40 });
-  colorData.push({ label: "green", y: 34 });
-  colorData.push({ label: "blue", y: 24 });
-  colorData.push({ label: "indigo", y: 24 });
-  colorData.push({ label: "violet", y: 24 });
-  colorData.push({ label: "black", y: 24 });
-  colorData.push({ label: "white", y: 24 });
-  colorData.push({ label: "gray", y: 24 });
+  colorData.push({ image: "images/red.jpg", label: "red", y: 18 });
+  colorData.push({ image: "images/orange.jpg", label: "orange", y: 29 });
+  colorData.push({ image: "images/yellow.jpg", label: "yellow", y: 40 });
+  colorData.push({ image: "images/green.jpg", label: "green", y: 34 });
+  colorData.push({ image: "images/blue.jpg", label: "blue", y: 24 });
+  colorData.push({ image: "images/indigo.jpg", label: "indigo", y: 24 });
+  colorData.push({ image: "images/violet.jpg", label: "violet", y: 24 });
+  colorData.push({ image: "images/black.jpg", label: "black", y: 24 });
+  colorData.push({ image: "images/white.jpg", label: "white", y: 24 });
+  colorData.push({ image: "images/gray.jpg", label: "gray", y: 24 });
   /***    Chart Object ***/
   var chart = null;
 
@@ -27,18 +27,20 @@
       var randomNum2 =  Math.floor((Math.random() * 10) + 1);
 
     /***Check to make sure the two numbers aren't the same***/
-      while (randomNum1 == randomNum2) {
-        randomNum1 = Math.floor((Math.random() * 10) + 1);
-        randomNum2 =  Math.floor((Math.random() * 10) + 1);
-      }
+       while (randomNum1 == randomNum2) {
+         randomNum1 = Math.floor((Math.random() * 10) + 1);
+         randomNum2 =  Math.floor((Math.random() * 10) + 1);
+       }
     /***Write the two choices to the page**/
       var choice1 = document.getElementById("choice1");
-      choice1.innerHTML = "<p>" + colorData[randomNum1].label + "</p>";
-      choice1.innerHTML+= "<button onclick='vote("+randomNum1+")'>Pick me</button>";
+      choice1.innerHTML= "";
+      choice1.innerHTML+= "<div><img src=' "+ colorData[randomNum1].image +" ' ></div>";
+      choice1.innerHTML+= "<button onclick='vote("+randomNum1+")'>"+colorData[randomNum1].label+"</button>";
 
       var choice2 = document.getElementById("choice2");
-      choice2.innerHTML = "<p>" + colorData[randomNum2].label + "</p>";
-      choice2.innerHTML+= "<button onclick='vote("+randomNum2+")'>Pick me</button>";
+      choice2.innerHTML= "";
+      choice2.innerHTML+= "<div><img src=' "+ colorData[randomNum2].image +" ' ></div>";
+      choice2.innerHTML+= "<button onclick='vote("+randomNum2+")'>"+colorData[randomNum2].label+"</button>";
   }
 
   window.onload = function () {
