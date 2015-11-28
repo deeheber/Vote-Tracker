a471bdd106c599bd05b93b7e891d4d5a5ee92bf4
Danielle H <daheber@gmail.com>
2015-11-28 12:34:50 -0800

    hide/show works

--- chart.js
+++ chart.js
@@ -1,17 +1,18 @@
 
   /***    Data Points ***/
   var colorData = new Array();
-  var red = { image: "images/red.jpg", label: "red", x: 1, y: 18 };
-  var orange = { image: "images/orange.jpg", label: "orange", x: 2, y: 29 };
-  var yellow = { image: "images/yellow.jpg", label: "yellow", x: 3, y: 40 };
-  var green = { image: "images/green.jpg", label: "green", x: 4, y: 34 };
-  var blue = { image: "images/blue.jpg", label: "blue", x: 5, y: 24 };
-  var indigo = { image: "images/indigo.jpg", label: "indigo", x: 6, y: 24 };
-  var violet = { image: "images/violet.jpg", label: "violet", x: 7, y: 24 };
-  var black = { image: "images/black.jpg", label: "black", x: 8, y: 24 };
-  var white = { image: "images/white.jpg", label: "white", x: 9, y: 24 };
-  var gray = { image: "images/gray.jpg", label: "gray", x: 10, y: 24 };
-  colorData.push(red, orange, yellow, green, blue, indigo, violet, black, white, gray);
+  var colorObject = {};
+  colorObject.red = { image: "images/red.jpg", label: "red", x: 1, y: 18 };
+  colorObject.orange = { image: "images/orange.jpg", label: "orange", x: 2, y: 29 };
+  colorObject.yellow = { image: "images/yellow.jpg", label: "yellow", x: 3, y: 40 };
+  colorObject.green = { image: "images/green.jpg", label: "green", x: 4, y: 34 };
+  colorObject.blue = { image: "images/blue.jpg", label: "blue", x: 5, y: 24 };
+  colorObject.indigo = { image: "images/indigo.jpg", label: "indigo", x: 6, y: 24 };
+  colorObject.violet = { image: "images/violet.jpg", label: "violet", x: 7, y: 24 };
+  colorObject.black = { image: "images/black.jpg", label: "black", x: 8, y: 24 };
+  colorObject.white = { image: "images/white.jpg", label: "white", x: 9, y: 24 };
+  colorObject.gray = { image: "images/gray.jpg", label: "gray", x: 10, y: 24 };
+  colorData.push(colorObject.red, colorObject.orange, colorObject.yellow, colorObject.green, colorObject.blue, colorObject.indigo, colorObject.violet, colorObject.black, colorObject.white, colorObject.gray);
   /***    Chart Object ***/
   var chart = null;
 
@@ -60,14 +61,24 @@
       }
     }
     if (selectedItem.checked == false) {
-       console.log(color +" is unchecked");
-       console.log(colorData.indexOf(selectedArrayItem));
+       //console.log(color +" is unchecked");
+       //console.log(colorData.indexOf(selectedArrayItem));
        colorData.splice(colorData.indexOf(selectedArrayItem), 1);
-       console.log(colorData);
+       //console.log(colorData);
+       for(index=0; index<colorData.length; index++) {
+          colorData[index].x = index+1;
+       }
        chart.render();
      }
      else {
        console.log(color + " is checked");
+       colorData.push(colorObject[color]);
+       for(index=0; index<colorData.length; index++) {
+          colorData[index].x = index+1;
+       }
+       console.log(colorData);
+       chart.render();
+
      }
     //If false - remove from array/redraw chart
     //if true - keep it in the array/redraw chart
