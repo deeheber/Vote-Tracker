07f68342ab8c06c7c0ba735a7bc75a56634cbe33
Danielle H <daheber@gmail.com>
2015-12-06 11:53:57 -0800

    commented out console logs

--- chart.js
+++ chart.js
@@ -117,28 +117,27 @@
 
   function getLocation(selectedArrayItem) {
     for(var index=0; index<masterList.length; index++){
-      console.log("checking Master List "+ index);
+      /*console.log("checking Master List "+ index);*/
       if(masterList[index] == selectedArrayItem){
-        console.log("found item");
+        /*console.log("found item");*/
         if(masterList[index-1]) {
-          console.log("there is an item before");
+          /*console.log("there is an item before");*/
           itemBefore = masterList[index-1];
-          console.log("item before is "+itemBefore.label);
+          /*console.log("item before is "+itemBefore.label);*/
           for(var counter=0; counter<colorData.length; counter++){
-            console.log("checking colorData "+ counter);
+            /*console.log("checking colorData "+ counter);*/
             if(colorData[counter] == itemBefore) {
-              console.log("item is showing");
+              /*console.log("item is showing");*/
               var xValue = colorData[counter].x;
               return xValue+1;
-              /**getting incorrect placement here originally index+1**/
             }
           }
-          console.log("hidden item");
+          /*console.log("hidden item");*/
           return getLocation(itemBefore);
 
         }
         else{
-          console.log("there is not an item before");
+          /*console.log("there is not an item before");*/
           return 1;
         }
       }
